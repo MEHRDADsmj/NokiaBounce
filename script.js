cc78fd4de6bc0f8ee9a26835792d8a279c5e1d0a
Mehrdad Jafari <asedmehrdad@gmail.com>
2022-05-27 16:40:13 +0430

    main script-TBI

---
+++ script.js
@@ -0,0 +1,36 @@
+// TBI
+var camera, scene, renderer;
+
+function Init()
+{
+    TBI();
+}
+
+// Three basic inits
+function TBI()
+{
+    scene = new THREE.Scene();
+
+    camera = new THREE.OrthographicCamera(window.innerWidth / 2, window.innerWidth / -2, window.innerHeight / 2, window.innerHeight / -2,
+        1, 1000);
+    camera.position.set(0.0, 0.0, -10.0);
+    camera.lookAt(scene.position);
+    scene.add(camera);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0xb4f7ee);
+    document.body.appendChild(renderer.domElement);
+}
+
+function Animate()
+{
+    requestAnimationFrame(Animate);
+    renderer.render(scene, camera);
+}
+
+window.onload = function ()
+{
+    Init();
+    Animate();
+}
